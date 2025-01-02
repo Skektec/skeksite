c51fd8a570665fc53977939b618ae7fff54aa9c2
Skektec <Skekung.yt@gmail.com>
2025-01-02 20:03:52 +0200

    rolled back

--- monitor.js
+++ monitor.js
@@ -1,29 +1,5 @@
-const os = require("os");
+let test = "TEST";
 
-function getCpuUsage() {
-  const cpus = os.cpus();
-  let totalIdle = 0,
-    totalTick = 0;
+let displayElement1 = document.getElementById("cpuse");
 
-  for (let i = 0; i < cpus.length; i++) {
-    const cpu = cpus[i];
-    for (type in cpu.times) {
-      totalTick += cpu.times[type];
-    }
-    totalIdle += cpu.times.idle;
-  }
-
-  const idle = totalIdle / cpus.length;
-  const total = totalTick / cpus.length;
-  const usage = 1 - idle / total;
-
-  return usage * 100;
-}
-
-setInterval(() => {
-  const cpuUsage = getCpuUsage();
-
-  let displayElement1 = document.getElementById("cpuse");
-
-  displayElement1.textContent = cpuUsage;
-}, 1000);
+displayElement1.textContent = test;
