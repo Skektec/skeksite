c12da7e434d4c506a7c3ef54ca2617ba00d88b19
Skektec <Skekung.yt@gmail.com>
2025-01-02 22:55:14 +0200

    Addeed dom content listener"

--- monitor.js
+++ monitor.js
@@ -1,21 +1,25 @@
-fetch("/api/temperature")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("cpuTemp").innerText = data.temperature;
-  })
-  .catch((err) => {
-    document.getElementById("cpuTemp").innerText = "Error";
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  // Fetch CPU temperature
+  fetch("/api/temperature")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("cpuTemp").innerText = data.temperature;
+    })
+    .catch((err) => {
+      document.getElementById("cpuTemp").innerText = "Error";
+    });
 
-fetch("/api/ram")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("ramUsed").innerText = data.usedMemory;
-    document.getElementById("ramFree").innerText = data.freeMemory;
-    document.getElementById("ramTotal").innerText = data.totalMemory;
-  })
-  .catch((err) => {
-    document.getElementById("ramUsed").innerText = "Error";
-    document.getElementById("ramFree").innerText = "Error";
-    document.getElementById("ramTotal").innerText = "Error";
-  });
+  // Fetch RAM usage data
+  fetch("/api/ram")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("ramUsed").innerText = data.usedMemory;
+      document.getElementById("ramFree").innerText = data.freeMemory;
+      document.getElementById("ramTotal").innerText = data.totalMemory;
+    })
+    .catch((err) => {
+      document.getElementById("ramUsed").innerText = "Error";
+      document.getElementById("ramFree").innerText = "Error";
+      document.getElementById("ramTotal").innerText = "Error";
+    });
+});
